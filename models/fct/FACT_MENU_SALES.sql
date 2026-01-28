a0c5f9e2c29c3a81d864fda23dfbad46c992c59c
bharath485 <bharath4507@gmail.com>
2026-01-28 17:30:19 +0530

    Auto-push dbt models for Copy_of_Copy_of_Source_to_Target_Mapping_2.xlsx | sheets: FACT_ORDERS

--- models/fct/FACT_MENU_SALES.sql
+++ models/fct/FACT_MENU_SALES.sql
@@ -2,25 +2,25 @@
 
 with menu_data as (
     select
-        cast(menu_item_id as integer) as menu_item_id,
+        cast(menu_item_id as varchar) as menu_item_id,
         cast(item_category as varchar) as item_category
     from {{ source('tb_101', 'MENU') }}
 ),
 
 order_detail_aggregated as (
     select
-        cast(menu_item_id as integer) as menu_item_id,
+        cast(menu_item_id as varchar) as menu_item_id,
         sum(cast(quantity as integer)) as quantity_sold,
         sum(cast(price as decimal(10,2))) as total_sales_amount
     from {{ source('tb_101', 'ORDER_DETAIL') }}
-    group by menu_item_id
+    group by cast(menu_item_id as varchar)
 )
 
 select
     m.menu_item_id as menu_item_key,
-    oda.quantity_sold,
-    oda.total_sales_amount,
+    coalesce(od.quantity_sold, 0) as quantity_sold,
+    coalesce(od.total_sales_amount, 0.00) as total_sales_amount,
     m.item_category
 from menu_data m
-left join order_detail_aggregated oda
-    on m.menu_item_id = oda.menu_item_id
+left join order_detail_aggregated od
+    on m.menu_item_id = od.menu_item_id
