16238d4263f9ea1b0877edf32bc81b2d99c629ab
bharath485 <bharath4507@gmail.com>
2026-01-28 16:59:38 +0530

    Auto-push dbt models for Copy_of_Copy_of_Source_to_Target_Mapping_2.xlsx | sheets: FACT_MENU_SALES

--- models/fct/FACT_MENU_SALES.sql
+++ models/fct/FACT_MENU_SALES.sql
@@ -2,14 +2,14 @@
 
 with menu_data as (
     select
-        cast(menu_item_id as varchar) as menu_item_id,
+        cast(menu_item_id as integer) as menu_item_id,
         cast(item_category as varchar) as item_category
     from {{ source('tb_101', 'MENU') }}
 ),
 
 order_detail_aggregated as (
     select
-        cast(menu_item_id as varchar) as menu_item_id,
+        cast(menu_item_id as integer) as menu_item_id,
         sum(cast(quantity as integer)) as quantity_sold,
         sum(cast(price as decimal(10,2))) as total_sales_amount
     from {{ source('tb_101', 'ORDER_DETAIL') }}
