161c1b4b4c5bc9ec6d6d061f7b6f73df46cfe09b
KBharath.10731399 <Kbharath.reddy@ltimindtree.com>
2026-03-05 20:08:05 +0530

    Update PO_DISTRIBUTION_HIST_DIM to use MERGE strategy for SCD2

--- models/cortex_generated/ETL_MART_PROCUREMENT_PO_DISTRIBUTION_HIST_DIM.sql
+++ models/cortex_generated/ETL_MART_PROCUREMENT_PO_DISTRIBUTION_HIST_DIM.sql
@@ -7,11 +7,13 @@ Command to run model:
 Version     Date            Author                Description
 -------     ----------      ----------------      ----------------------------------
 1.0         2026-03-05      Cortex Code           Initial Version - Auto-Generated from STTM
+1.1         2026-03-05      Cortex Code           Updated to MERGE load strategy for SCD2
 
 ----------------------------------------------------------------------------------------------*/
 
 {################# EDW Job Template Variables #################}
-{%-set v_pk_list = ['PO_DISTRIBUTION_KEY', 'DBT_SCD_ID']-%}
+{%-set v_pk_list = ['PO_DISTRIBUTION_KEY']-%}
+{%-set v_scd_key = 'DBT_SCD_ID'-%}
 {% if is_incremental() %}
 {%-set v_house_keeping_column = ['BIW_INS_DTTM','BIW_UPD_DTTM','BIW_BATCH_ID','BIW_MD5_KEY']-%}
 {%-set v_all_column_list =  edw_get_column_list( this ) -%}
@@ -31,13 +33,15 @@ Version     Date            Author                Description
 {################# Snowflake Object Configuration #################}
 {{
     config(
-        description = 'Building ETL MART SCD2 snapshot table for PO_DISTRIBUTION_HIST_DIM',
+        description = 'Building ETL MART SCD2 snapshot table for PO_DISTRIBUTION_HIST_DIM using MERGE strategy',
         transient=true,
-        materialized='table',
+        materialized='incremental',
+        incremental_strategy='merge',
         schema ='ETL_MART_PROCUREMENT',
         alias='PO_DISTRIBUTION_HIST_DIM',
         tags =['MART_PROCUREMENT'],
-        unique_key= v_pk_list,
+        unique_key= v_scd_key,
+        merge_update_columns = ['DBT_VALID_TO', 'BIW_UPD_DTTM', 'BIW_BATCH_ID'],
         post_hook= [v_sql_upd_success_batch]  
     )
 }}
@@ -45,6 +49,7 @@ Version     Date            Author                Description
 {################# CTE Definitions #################}
 
 WITH PO_DISTRIBUTION_DIM AS (
+    -- Source: Current dimension table with changed/new records
     SELECT 
         PO_DISTRIBUTION_KEY,
         PO_DISTRIBUTION_ID,
@@ -106,69 +111,165 @@ WITH PO_DISTRIBUTION_DIM AS (
         {% endif %}
 )
 
-{################# Final SELECT #################}
+{% if is_incremental() %}
+-- Records to CLOSE (existing active records that have changed)
+,RECORDS_TO_CLOSE AS (
+    SELECT 
+        TGT.DBT_SCD_ID,
+        '{{V_START_DTTM}}'::DATE AS DBT_VALID_TO,
+        '{{V_START_DTTM}}'::TIMESTAMP_NTZ AS BIW_UPD_DTTM,
+        '{{V_BIW_BATCH_ID}}'::NUMBER(38,0) AS BIW_BATCH_ID
+    FROM {{ this }} TGT
+    INNER JOIN PO_DISTRIBUTION_DIM SRC
+        ON TGT.PO_DISTRIBUTION_KEY = SRC.PO_DISTRIBUTION_KEY
+    WHERE TGT.DBT_VALID_TO = '9999-12-31'::DATE
+      AND TGT.BIW_MD5_KEY != SRC.BIW_MD5_KEY
+)
+{% endif %}
 
+-- New records to INSERT (new versions of changed records + brand new records)
+,RECORDS_TO_INSERT AS (
+    SELECT 
+        -- Business Columns
+        SRC.PO_DISTRIBUTION_KEY,
+        SRC.PO_DISTRIBUTION_ID,
+        SRC.DISTRIBUTION_NUM,
+        SRC.PO_HEADER_ID,
+        SRC.PO_LINE_ID,
+        SRC.LINE_LOCATION_ID,
+        SRC.REQ_DISTRIBUTION_ID,
+        SRC.WIP_ENTITY_ID,
+        SRC.BUDGET_DATE,
+        SRC.CANCEL_BUDGET_DATE,
+        SRC.CLOSE_BUDGET_DATE,
+        SRC.CREATION_DATE,
+        SRC.LAST_UPDATE_DATE,
+        SRC.RATE_DATE,
+        SRC.SET_OF_BOOKS_ID,
+        SRC.DELIVER_TO_LOCATION_ID,
+        SRC.CODE_COMBINATION_ID,
+        SRC.DELIVER_TO_PERSON_ID,
+        SRC.REQ_HEADER_REFERENCE_NUM,
+        SRC.REQ_LINE_REFERENCE_NUM,
+        SRC.PRC_BU_ID,
+        SRC.REQ_BU_ID,
+        SRC.WIP_LINE_ID,
+        SRC.DESTINATION_ORGANIZATION_ID,
+        SRC.SOLDTO_BU_ID,
+        SRC.DESTINATION_TYPE_CODE,
+        SRC.RATE,
+        SRC.DESTINATION_SUBINVENTORY,
+        SRC.DESTINATION_CONTEXT,
+        SRC.DISTRIBUTION_TYPE,
+        SRC.INVOICE_ADJUSTMENT_FLAG,
+        SRC.ACCRUED_FLAG,
+        SRC.ENCUMBERED_FLAG,
+        SRC.ACCRUE_ON_RECEIPT_FLAG,
+        SRC.PJC_PROJECT_ID,
+        SRC.PJC_TASK_ID,
+        SRC.PROJECT_NUMBER,
+        SRC.PROJECT_NAME,
+        SRC.TASK_NUMBER,
+        SRC.TASK_NAME,
+        SRC.WIP_OPERATION_SEQ_NUM,
+        SRC.WIP_REPETITIVE_SCHEDULE_ID,
+        SRC.WIP_RESOURCE_SEQ_NUM,
+        SRC.PREVENT_ENCUMBRANCE_FLAG,
+        
+        -- Housekeeping Columns
+        SRC.IS_DELETE,
+        '{{V_START_DTTM}}'::TIMESTAMP_NTZ AS BIW_INS_DTTM,
+        '{{V_START_DTTM}}'::TIMESTAMP_NTZ AS BIW_UPD_DTTM,
+        '{{V_BIW_BATCH_ID}}'::NUMBER(38,0) AS BIW_BATCH_ID,
+        SRC.BIW_MD5_KEY,
+        
+        -- SCD2 Columns
+        MD5(OBJECT_CONSTRUCT(
+            'COL1', SRC.PO_DISTRIBUTION_KEY::STRING,
+            'COL2', '{{V_START_DTTM}}'::STRING
+        )::STRING)::STRING AS DBT_SCD_ID,
+        '{{V_START_DTTM}}'::DATE AS DBT_UPDATED_AT,
+        '{{V_START_DTTM}}'::DATE AS DBT_VALID_FROM,
+        '9999-12-31'::DATE AS DBT_VALID_TO
+    FROM PO_DISTRIBUTION_DIM SRC
+    {% if is_incremental() %}
+    -- Only insert if: new record OR record has changed (MD5 mismatch)
+    WHERE NOT EXISTS (
+        SELECT 1 FROM {{ this }} TGT
+        WHERE TGT.PO_DISTRIBUTION_KEY = SRC.PO_DISTRIBUTION_KEY
+          AND TGT.DBT_VALID_TO = '9999-12-31'::DATE
+          AND TGT.BIW_MD5_KEY = SRC.BIW_MD5_KEY
+    )
+    {% endif %}
+)
+
+{################# Final SELECT - MERGE Pattern #################}
+
+{% if is_incremental() %}
+-- UNION: Closed records (updates) + New records (inserts)
 SELECT 
-    -- Business Columns (from source DIM)
-    SRC.PO_DISTRIBUTION_KEY,
-    SRC.PO_DISTRIBUTION_ID,
-    SRC.DISTRIBUTION_NUM,
-    SRC.PO_HEADER_ID,
-    SRC.PO_LINE_ID,
-    SRC.LINE_LOCATION_ID,
-    SRC.REQ_DISTRIBUTION_ID,
-    SRC.WIP_ENTITY_ID,
-    SRC.BUDGET_DATE,
-    SRC.CANCEL_BUDGET_DATE,
-    SRC.CLOSE_BUDGET_DATE,
-    SRC.CREATION_DATE,
-    SRC.LAST_UPDATE_DATE,
-    SRC.RATE_DATE,
-    SRC.SET_OF_BOOKS_ID,
-    SRC.DELIVER_TO_LOCATION_ID,
-    SRC.CODE_COMBINATION_ID,
-    SRC.DELIVER_TO_PERSON_ID,
-    SRC.REQ_HEADER_REFERENCE_NUM,
-    SRC.REQ_LINE_REFERENCE_NUM,
-    SRC.PRC_BU_ID,
-    SRC.REQ_BU_ID,
-    SRC.WIP_LINE_ID,
-    SRC.DESTINATION_ORGANIZATION_ID,
-    SRC.SOLDTO_BU_ID,
-    SRC.DESTINATION_TYPE_CODE,
-    SRC.RATE,
-    SRC.DESTINATION_SUBINVENTORY,
-    SRC.DESTINATION_CONTEXT,
-    SRC.DISTRIBUTION_TYPE,
-    SRC.INVOICE_ADJUSTMENT_FLAG,
-    SRC.ACCRUED_FLAG,
-    SRC.ENCUMBERED_FLAG,
-    SRC.ACCRUE_ON_RECEIPT_FLAG,
-    SRC.PJC_PROJECT_ID,
-    SRC.PJC_TASK_ID,
-    SRC.PROJECT_NUMBER,
-    SRC.PROJECT_NAME,
-    SRC.TASK_NUMBER,
-    SRC.TASK_NAME,
-    SRC.WIP_OPERATION_SEQ_NUM,
-    SRC.WIP_REPETITIVE_SCHEDULE_ID,
-    SRC.WIP_RESOURCE_SEQ_NUM,
-    SRC.PREVENT_ENCUMBRANCE_FLAG,
-    
-    -- Housekeeping Columns
-    SRC.IS_DELETE,
-    SRC.BIW_INS_DTTM,
-    '{{V_START_DTTM}}'::TIMESTAMP_NTZ AS BIW_UPD_DTTM,
-    '{{V_BIW_BATCH_ID}}'::NUMBER(38,0) AS BIW_BATCH_ID,
-    SRC.BIW_MD5_KEY,
-    
-    -- SCD2 Columns
-    MD5(OBJECT_CONSTRUCT(
-        'COL1', SRC.PO_DISTRIBUTION_KEY::STRING,
-        'COL2', '{{V_START_DTTM}}'::STRING
-    )::STRING)::STRING AS DBT_SCD_ID,
-    '{{V_START_DTTM}}'::DATE AS DBT_UPDATED_AT,
-    '{{V_START_DTTM}}'::DATE AS DBT_VALID_FROM,
-    '9999-12-31'::DATE AS DBT_VALID_TO
+    TGT.PO_DISTRIBUTION_KEY,
+    TGT.PO_DISTRIBUTION_ID,
+    TGT.DISTRIBUTION_NUM,
+    TGT.PO_HEADER_ID,
+    TGT.PO_LINE_ID,
+    TGT.LINE_LOCATION_ID,
+    TGT.REQ_DISTRIBUTION_ID,
+    TGT.WIP_ENTITY_ID,
+    TGT.BUDGET_DATE,
+    TGT.CANCEL_BUDGET_DATE,
+    TGT.CLOSE_BUDGET_DATE,
+    TGT.CREATION_DATE,
+    TGT.LAST_UPDATE_DATE,
+    TGT.RATE_DATE,
+    TGT.SET_OF_BOOKS_ID,
+    TGT.DELIVER_TO_LOCATION_ID,
+    TGT.CODE_COMBINATION_ID,
+    TGT.DELIVER_TO_PERSON_ID,
+    TGT.REQ_HEADER_REFERENCE_NUM,
+    TGT.REQ_LINE_REFERENCE_NUM,
+    TGT.PRC_BU_ID,
+    TGT.REQ_BU_ID,
+    TGT.WIP_LINE_ID,
+    TGT.DESTINATION_ORGANIZATION_ID,
+    TGT.SOLDTO_BU_ID,
+    TGT.DESTINATION_TYPE_CODE,
+    TGT.RATE,
+    TGT.DESTINATION_SUBINVENTORY,
+    TGT.DESTINATION_CONTEXT,
+    TGT.DISTRIBUTION_TYPE,
+    TGT.INVOICE_ADJUSTMENT_FLAG,
+    TGT.ACCRUED_FLAG,
+    TGT.ENCUMBERED_FLAG,
+    TGT.ACCRUE_ON_RECEIPT_FLAG,
+    TGT.PJC_PROJECT_ID,
+    TGT.PJC_TASK_ID,
+    TGT.PROJECT_NUMBER,
+    TGT.PROJECT_NAME,
+    TGT.TASK_NUMBER,
+    TGT.TASK_NAME,
+    TGT.WIP_OPERATION_SEQ_NUM,
+    TGT.WIP_REPETITIVE_SCHEDULE_ID,
+    TGT.WIP_RESOURCE_SEQ_NUM,
+    TGT.PREVENT_ENCUMBRANCE_FLAG,
+    TGT.IS_DELETE,
+    TGT.BIW_INS_DTTM,
+    CLS.BIW_UPD_DTTM,
+    CLS.BIW_BATCH_ID,
+    TGT.BIW_MD5_KEY,
+    CLS.DBT_SCD_ID,
+    TGT.DBT_UPDATED_AT,
+    TGT.DBT_VALID_FROM,
+    CLS.DBT_VALID_TO
+FROM {{ this }} TGT
+INNER JOIN RECORDS_TO_CLOSE CLS
+    ON TGT.DBT_SCD_ID = CLS.DBT_SCD_ID
 
-FROM PO_DISTRIBUTION_DIM SRC
+UNION ALL
+
+SELECT * FROM RECORDS_TO_INSERT
+
+{% else %}
+-- Full refresh: Insert all records
+SELECT * FROM RECORDS_TO_INSERT
+{% endif %}
